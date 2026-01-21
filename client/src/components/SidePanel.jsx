14e061be10161e569c667243e2b5071946c535db
최우인 <choiwoo02in@gmail.com>
2026-01-21 14:16:51 +0900

    시간대 재설정

--- client/src/components/SidePanel.jsx
+++ client/src/components/SidePanel.jsx
@@ -28,11 +28,13 @@ function SidePanel({ isOpen, onClose, currentUser }) {
   const formatDate = (dateString) => {
     const date = new Date(dateString)
     return date.toLocaleString('ko-KR', {
+      timeZone: 'Asia/Seoul',
       year: 'numeric',
       month: '2-digit',
       day: '2-digit',
       hour: '2-digit',
       minute: '2-digit',
+      hour12: false,
     })
   }
 
